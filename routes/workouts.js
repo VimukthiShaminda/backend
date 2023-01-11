5a7d5ab1ccd45e12175d494eebf4fb8f8a2f38ea
vimukthi871 <vimukthishaminda871@gmail.com>
2023-01-12 01:55:53 +0530

    backend

---
+++ routes/workouts.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const {
+    createWorkout,
+    getWorkout,
+    getWorkouts,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutsController')
+const router = express.Router()
+
+//GET all workouts
+router.get('/', getWorkouts)
+
+//GET a single workout
+router.get('/:id',getWorkout)
+
+//POST a new  workout
+router.post('/',createWorkout)
+
+//DELETE a workout
+router.delete('/:id',deleteWorkout)
+
+//UPDATE a workouts
+router.patch('/:id',updateWorkout)
+
+module.exports = router
