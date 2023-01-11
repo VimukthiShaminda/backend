aa2ffa747033d9696465165bdfc3bf00e74dbf35
vimukthi871 <vimukthishaminda871@gmail.com>
2023-01-12 02:15:48 +0530

    backend

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -13,7 +13,7 @@ const getWorkout = async (req,res)=>{
     const {id} = req.params
 
     if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such workout'})
+        return res.status(404).json({error: 'No such workout!'})
     }
     const workout = await Workout.findById(id)
 
